49826f13fb65a3efea006ae1b8add566a7e703e6
DiorginesLacerda <diorgineslacerda@gmail.com>
2018-12-09 18:26:52 -0200

    projeto criado

---
+++ src/main/java/br/edu/qi/planetoffice/PlanetOfficeApplication.java
@@ -0,0 +1,12 @@
+package br.edu.qi.planetoffice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanetOfficeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanetOfficeApplication.class, args);
+	}
+}
